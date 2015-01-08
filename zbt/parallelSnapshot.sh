3db9cec7acc4b551e0435d6e4f1d284339f24222
jwagner313 <oper@StationUnit7.haystack.mit.edu>
2015-01-07 22:18:36 -0500

    fixed m5time arguments

--- zbt/parallelSnapshot.sh
+++ zbt/parallelSnapshot.sh
@@ -18,20 +18,21 @@ sleep 2
 #  DBBC  sends 2 x 16x62.5 MHz  = 2000 MHz (two different IFs, 0-1024MHz/1000MHz and 1200-1800MHz/1000MHz)
 ssh oper@$MK5C_IP_1 '/usr/local/bin/vdifsnapshotUDP --cpu=2  --offset=8 4096 4001 /home/oper/vdif/r2dbe.vdif'  &
 ssh oper@$MK5C_IP_2 '/usr/local/bin/vdifsnapshotUDP --cpu=2  --offset=8 2048 46227 /home/oper/vdif/dbbc.vdif'
+fg
 
 # Split out threads from recorded data when recording dual threads (e.g. from two 10 GbE interfaces)
-ssh oper@$MK5C_IP_1 '/usr/local/bin/modifyVDIF --extract=0 /home/oper/vdif/r2dbe.vdif /home/oper/vdif/r2dbe_t0.vdif' &
+ssh oper@$MK5C_IP_1 '/usr/local/bin/modifyVDIF --extract=0 /home/oper/vdif/r2dbe.vdif /home/oper/vdif/r2dbe_t0.vdif' 
 ssh oper@$MK5C_IP_1 '/usr/local/bin/modifyVDIF --extract=1 /home/oper/vdif/r2dbe.vdif /home/oper/vdif/r2dbe_t1.vdif'
-ssh oper@$MK5C_IP_2 '/usr/local/bin/modifyVDIF --extract=0 /home/oper/vdif/dbbc.vdif /home/oper/vdif/dbbc_t0.vdif' &
+ssh oper@$MK5C_IP_2 '/usr/local/bin/modifyVDIF --extract=0 /home/oper/vdif/dbbc.vdif /home/oper/vdif/dbbc_t0.vdif'
 ssh oper@$MK5C_IP_2 '/usr/local/bin/modifyVDIF --extract=1 /home/oper/vdif/dbbc.vdif /home/oper/vdif/dbbc_t1.vdif'
 
 # Show the time stamps (can be copied into DiFX):
 echo "Time info for /home/oper/vdif/r2dbe_t0.vdif:"
-ssh oper@$MK5C_IP_1 '/usr/local/difx/bin/m5time VDIF_8192-8192-1-2 /home/oper/vdif/r2dbe_t0.vdif'
+ssh oper@$MK5C_IP_1 '/usr/local/difx/bin/m5time /home/oper/vdif/r2dbe_t0.vdif VDIF_8192-8192-1-2'
 echo "Time info for /home/oper/vdif/r2dbe_t1.vdif:"
-ssh oper@$MK5C_IP_1 '/usr/local/difx/bin/m5time VDIF_8192-8192-1-2 /home/oper/vdif/r2dbe_t1.vdif'
+ssh oper@$MK5C_IP_1 '/usr/local/difx/bin/m5time /home/oper/vdif/r2dbe_t1.vdif VDIF_8192-8192-1-2'
 
 echo "Time info for /home/oper/vdif/dbbc_t0.vdif:"
-ssh oper@$MK5C_IP_2 '/usr/local/difx/bin/m5time VDIF_8000-4000-16-2 /home/oper/vdif/dbbc_t0.vdif'
+ssh oper@$MK5C_IP_2 '/usr/local/difx/bin/m5time /home/oper/vdif/dbbc_t0.vdif VDIF_8000-4000-16-2'
 echo "Time info for /home/oper/vdif/dbbc_t1.vdif:"
-ssh oper@$MK5C_IP_2 '/usr/local/difx/bin/m5time VDIF_8000-4000-16-2 /home/oper/vdif/dbbc_t1.vdif'
+ssh oper@$MK5C_IP_2 '/usr/local/difx/bin/m5time /home/oper/vdif/dbbc_t1.vdif VDIF_8000-4000-16-2'
