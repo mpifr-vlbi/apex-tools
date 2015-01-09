4835926a376c68defaee661db2bd149838b3170f
oper <oper@StationUnit7.haystack.mit.edu>
2015-01-09 08:48:12 -0500

    del some unused files

--- APECS/2015/copyToBonn.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-tar czvf vlbi_scripts_21march2013.tar.gz * --exclude code_revisions --exclude="*.tar.gz" --exclude="*.avi"
-mv vlbi_scripts_21march2013.tar.gz code_revisions/
-
-rsync -Laxv --exclude "*.avi" ~/* jwagner@portal.mpifr-bonn.mpg.de:/homes/jwagner/apex/apecs/2013/
-
