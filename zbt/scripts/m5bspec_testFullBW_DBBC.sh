8c66981ff6a60b53fdd31285a12fc676e3e5f623
jwagner313 <oper@StationUnit7.haystack.mit.edu>
2015-01-10 15:20:01 -0500

    DBBC 16x62.5 MHz and 2000 MHz spectrum grab script update

--- zbt/scripts/m5bspec_testFullBW_DBBC.sh
+++ zbt/scripts/m5bspec_testFullBW_DBBC.sh
@@ -1,4 +1,4 @@
-# For DBBC, 16 x 62.5 MHz mode
+# For DBBC, 1 x 2000 MHz mode
 
 rm -f m5spec_test.vdif m5spec_test_t0.vdif m5spec_test_t1.vdif \
 	m5spec_test_t0.m5spec m5spec_test_t1.m5spec \
@@ -8,8 +8,8 @@ vdifsnapshotUDP --cpu=2 --offset=8 128 46227 m5spec_test.vdif
 modifyVDIF --extract=0 m5spec_test.vdif m5spec_test_t0.vdif
 modifyVDIF --extract=1 m5spec_test.vdif m5spec_test_t1.vdif
 
-m5spec -nopol m5spec_test_t0.vdif VDIF_8000-4000-16-2 8192 2000 m5spec_test_t0.m5spec
-m5spec -nopol m5spec_test_t1.vdif VDIF_8000-4000-16-2 8192 2000 m5spec_test_t1.m5spec
+m5spec -nopol m5spec_test_t0.vdif VDIF_8000-4000-1-2 32768 2000 m5spec_test_t0.m5spec
+m5spec -nopol m5spec_test_t1.vdif VDIF_8000-4000-1-2 32768 2000 m5spec_test_t1.m5spec
 
 echo "m5specGlueplot('m5spec_test_t0.m5spec')"> ./m5spec_test.m
 octave ./m5spec_test.m
