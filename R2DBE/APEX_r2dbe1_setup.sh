e9d4d0798349fdb5ceda1d1e4710c5d9987a97b7
oper <oper@StationUnit7.haystack.mit.edu>
2017-09-18 13:17:25 +0000

    add --pols and assume each R2DBE receives a single pol

--- R2DBE/APEX_r2dbe1_setup.sh
+++ R2DBE/APEX_r2dbe1_setup.sh
@@ -3,8 +3,8 @@
 #
 
 # If Mark6 (vlbi1) network cards change re-run this
-#   /etc/r2dbe/R2DBE_config.py --ifaces eth3,eth5 --stationIDs AR,ar --src 192.168.1.13,192.168.1.15 --dst 192.168.1.3,192.168.1.5 r2dbe-1
+#   /etc/r2dbe/R2DBE_config.py --ifaces eth3,eth5 --src 192.168.1.13,192.168.1.15 --dst 192.168.1.3,192.168.1.5 r2dbe-1
 # to determine the new MAC addresses to use here:
-/etc/r2dbe/R2DBE_config.py --ifaces eth3,eth5 --stationIDs AR,ar --src 192.168.1.13,192.168.1.15 --dst 192.168.1.3,192.168.1.5 --arp 3,0060dd440b75 --arp 5,0060dd4446c0 r2dbe-1
+/etc/r2dbe/R2DBE_config.py --ifaces eth3,eth5 --stationIDs AR,ar --src 192.168.1.13,192.168.1.15 --dst 192.168.1.3,192.168.1.5 --arp 3,0060dd440b75 --arp 5,0060dd4446c0 --pols 0,0 r2dbe-1
 
 
--- R2DBE/APEX_r2dbe2_setup.sh
+++ R2DBE/APEX_r2dbe2_setup.sh
@@ -3,8 +3,8 @@
 #
 
 # If Mark6 (vlbi2) network cards change re-run this
-#   /etc/r2dbe/R2DBE_config.py --ifaces eth3,eth5 --stationIDs AR,ar --src 192.168.1.13,192.168.1.15 --dst 192.168.1.3,192.168.1.5 r2dbe-2
+#   /etc/r2dbe/R2DBE_config.py --ifaces eth3,eth5 --src 192.168.1.13,192.168.1.15 --dst 192.168.1.3,192.168.1.5 r2dbe-2
 # to determine the new MAC addresses to use here:
-/etc/r2dbe/R2DBE_config.py --ifaces eth3,eth5 --stationIDs AR,ar --src 192.168.1.13,192.168.1.15 --dst 192.168.1.3,192.168.1.5 --arp 3,0060dd444639 --arp 5,0060dd444643 r2dbe-2
+/etc/r2dbe/R2DBE_config.py --ifaces eth3,eth5 --stationIDs AR,ar --src 192.168.1.13,192.168.1.15 --dst 192.168.1.3,192.168.1.5 --arp 3,0060dd444639 --arp 5,0060dd444643 --pols 1,1 r2dbe-2
 
 
