cc85c4cb3874a1c6f458767b43e796617049555a
APEX oper acct <you@example.com>
2017-09-22 13:15:27 -0300

    read disk temps

--- sensors/monitorMark6.py
+++ sensors/monitorMark6.py
@@ -27,7 +27,7 @@ def filter(lines,keys):
 				break
 	return out
 
-def readoutTemps():
+def readoutMark6s():
 	clear = '\033c'
 	colcycle = [Fore.GREEN, Fore.MAGENTA, Fore.CYAN, Fore.BLUE]
 	iter = 0
@@ -45,17 +45,61 @@ def readoutTemps():
 
 		# Readings from hosts
 		for h in hosts:
+
+			# Temperatures
 			cmd = '/usr/bin/ssh %s /usr/local/bin/sensors' % (h)
+			got_sensors = False
 			try:
 				s = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+				got_sensors = True
 			except Exception as e:
+				#print str(e)
 				next_out.append(Fore.RED + Back.BLACK + '%-10s : '%(h) + str(e))
-				n = (n + 1) % len(colcycle)
-				continue
-			l = s.split('\n')
-			fl = filter(l, temperatures)
-			for line in fl:
-				next_out.append(colcycle[n] + Back.BLACK + '%-10s : '%(h) + Fore.RESET + line)
+			if got_sensors:
+				l = s.split('\n')
+				fl = filter(l, temperatures)
+				for line in fl:
+					next_out.append(colcycle[n] + Back.BLACK + '%-10s : '%(h) + Fore.RESET + line)
+
+			# Disks
+			cmd = '/usr/bin/ssh %s /bin/ls -l /dev/disk/by-path/ | /bin/grep sas | /bin/grep -v part' % (h)
+			got_disks = False
+			try:
+				s = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+				got_disks = True
+			except Exception as e:
+				#print str(e)
+				pass
+			disks = []
+			if got_disks:
+				for l in s.split('\n'):
+					i = l.rfind('/')
+					if i < 0:
+						continue
+					dev = l[(i+1):]
+					disks.append(dev)
+				summary = '%d disks' % (len(disks))
+				next_out.append(colcycle[n] + Back.BLACK + '%-10s : '%(h) + Fore.RESET + summary)
+
+			# Disk temperatures
+			temps = []
+			for disk in disks:
+				cmd = '/usr/bin/ssh %s "sudo /usr/sbin/smartctl -A /dev/%s | /bin/grep Temperature_Celsius"' % (h,disk)
+				try:
+					s = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
+				except Exception as e:
+					#print str(e)
+					continue
+				vals = s.split()
+				#print vals, vals[9]
+				temps.append(int(vals[9]))
+			if len(disks)>0:
+				# summary = 'disk temps ' + ' '.join([str(t) for t in temps])
+				M = sum(temps)/len(temps)
+				summary = 'disk temperatures %dC min %dC mean %dC max' % (min(temps),M,max(temps))
+				next_out.append(colcycle[n] + Back.BLACK + '%-10s : '%(h) + Fore.RESET + summary)
+
+			# Next color code
 			n = (n + 1) % len(colcycle)
 
 		# Now print
@@ -65,5 +109,6 @@ def readoutTemps():
 		time.sleep(5)
 		iter = iter + 1
 
-readoutTemps()
+
+readoutMark6s()
 
