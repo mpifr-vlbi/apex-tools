95bb9e0d0139e58fe1c1fb96a16f653eca657cf9
APEX oper acct <you@example.com>
2017-09-22 08:52:03 -0300

    buffer before output

--- sensors/monitorMark6.py
+++ sensors/monitorMark6.py
@@ -1,11 +1,14 @@
 #!/usr/bin/python
+"""
+Poll Mark6 machines and read their 'lmsensors'.
+"""
 
 import subprocess, time
 from datetime import datetime
 from colorama import Fore, Back, Style
 
 hosts = ['vlbi1', 'vlbi2', 'vlbi3', 'vlbi4' ]
-temperatures = ['Physical', 'Core', 'temp', 'SYSTIN', 'CPUTIN', 'AUXTIN', 'fan2']
+temperatures = ['Physical', 'Core', 'temp', 'CPUTIN', 'SYSTIN', 'AUXTIN', 'fan2']
 
 def filter(lines,keys):
 	out = []
@@ -29,25 +32,37 @@ def readoutTemps():
 	colcycle = [Fore.GREEN, Fore.MAGENTA, Fore.CYAN, Fore.BLUE]
 	iter = 0
 	while True:
+
+		# Collect new printouts (slowly) before actually printing
+		next_out = []
+
+		# Header
 		T = datetime.utcnow()
 		title = '%s refresh %d' % (str(T),iter)
-		print clear
-		print (Back.BLUE + Fore.WHITE + '  %-70s  ' % (title))
+		next_out.append(clear)
+		next_out.append(Back.BLUE + Fore.WHITE + '  %-70s  ' % (title))
 		n = 0
+
+		# Readings from hosts
 		for h in hosts:
 			cmd = '/usr/bin/ssh %s /usr/local/bin/sensors' % (h)
 			try:
 				s = subprocess.check_output(cmd, stderr=subprocess.STDOUT, shell=True)
 			except Exception as e:
-				print (Fore.RED + Back.BLACK + '%-10s : '%(h) + str(e))
+				next_out.append(Fore.RED + Back.BLACK + '%-10s : '%(h) + str(e))
 				n = (n + 1) % len(colcycle)
 				continue
 			l = s.split('\n')
 			fl = filter(l, temperatures)
 			for line in fl:
-				print (colcycle[n] + Back.BLACK + '%-10s : '%(h) + Fore.RESET + line)
+				next_out.append(colcycle[n] + Back.BLACK + '%-10s : '%(h) + Fore.RESET + line)
 			n = (n + 1) % len(colcycle)
-		time.sleep(10)
+
+		# Now print
+		for l in next_out:
+			print l
+
+		time.sleep(5)
 		iter = iter + 1
 
 readoutTemps()
