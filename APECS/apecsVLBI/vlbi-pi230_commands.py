a1a836fb6d4164e69c1b7cb3ada5b3d8067190d9
jwagner <jan.wagner@iki.fi>
2018-04-23 10:01:05 -0300

    updated to the 2018 version

---
+++ APECS/apecsVLBI/vlbi-pi230_commands.py
@@ -0,0 +1,102 @@
+import apexObsUtils
+import socket
+import sys
+import subprocess
+import datetime
+import os
+
+#############################################################################
+# VLBI Calibration Helpers
+#############################################################################
+
+# vlbi_tpoint()              Total power pointing for VLBI
+# vlbi_tp_onsource()         Total power pointing for VLBI, taken while tracking source
+# vlbi_wpoint(t=20,cal=1)    Wobbler pointing for VLBI
+# vlbi_focus(axis='Z')       Focus scan for VLBI
+# vlbi_initiate_tsys()       Start a calibration (Tsys measurement)
+# vlbi_get_calibration()     Collect calibration results
+
+def vlbi_tpoint():
+    '''Total power pointing for VLBI.'''
+    tp()
+    point(150, time=20)
+
+def vlbi_tp_onsource_prepare(src='M87'):
+    execfile('pi230_setup.apecs')
+    source(src)
+    # Try this...JPE @ 2018-04-19
+    tp()
+    repeat(1)
+    use_ref('ON')
+    reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='EQ', epoch=2000.0)
+    use_ref('OFF')  
+
+def vlbi_tp_onsource(src='M87',t=4):
+    '''Total power monitoring for VLBI, taken while tracking source.
+    
+       t : integration time in MINUTES.
+    '''
+    pi230.configure(doppler='off') 
+    repeat(t)
+    on(drift='no',time=60)
+    repeat(1)
+    track()
+    # Now get Tsys
+    calibrate(time=5)
+    #use_ref('ON')
+
+def vlbi_tp_onsource_Old(src='M87',t=4):
+    '''Total power monitoring for VLBI, taken while tracking source.
+    
+       t : integration time in MINUTES.
+    '''
+    execfile('pi230_setup.apecs')
+    source(src)
+    #track()   # probably should be changed to "point(<args>)"
+    #on(drift='no',time=30)
+    # Try this...JPE @ 2018-04-19   
+    tp()    
+    repeat(1)
+    use_ref('ON')       
+    reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='EQ', epoch=2000.0)
+    use_ref('OFF')  
+    pi230.configure(doppler='off') 
+    repeat(t)
+    on(drift='no',time=60)
+    repeat(1)
+    track()
+    calibrate(time=5)
+    #use_ref('ON')    
+    
+
+def vlbi_wpoint(t=20,cal=1):
+    '''Wobbler pointing for VLBI.'''
+    if (cal):
+       calibrate('cold')
+
+    wob(75, 1)
+    #point(150, time=t)
+    point(length=54, unit='arcsec', time=t, mode='ras', points=5, direction='x')
+    tp()
+
+def vlbi_focus(axis='Z'):
+    '''Focus scan for VLBI.'''
+    vlbi_focus.func_defaults = (axis,)
+    wob(75, 1)
+    focus(amplitude=1, points=5, axis=axis)
+    tp()
+
+def vlbi_initiate_tsys():
+    '''Start a calibration (Tsys measurement).'''
+    #reference (-1000,0)
+    #calibrate()
+    #reference (0,0)
+    pass
+
+def vlbi_get_calibration():
+    '''Collect calibration results'''
+    onlineCal = apexObsUtils.getApexCalibrator()
+    try:
+        calResult = onlineCal.getCalResult('PI230-PBE_C',1,0)
+    except:
+        print 'No calibration result available.'
