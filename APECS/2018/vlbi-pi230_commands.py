0ee52d6066e11b1afcd6a94bc0938bfdf84e60e6
jwagner <jan.wagner@iki.fi>
2018-04-24 18:56:25 -0300

    update to new APECS commands

--- APECS/2018/vlbi-pi230_commands.py
+++ APECS/2018/vlbi-pi230_commands.py
@@ -6,22 +6,37 @@ import datetime
 import os
 
 #############################################################################
-# VLBI Calibration Helpers
+# VLBI Calibration and Scan/VLBI Recording Helpers
 #############################################################################
 
-# vlbi_tpoint()              Total power pointing for VLBI
-# vlbi_tp_onsource()         Total power pointing for VLBI, taken while tracking source
-# vlbi_wpoint(t=20,cal=1)    Wobbler pointing for VLBI
-# vlbi_focus(axis='Z')       Focus scan for VLBI
-# vlbi_initiate_tsys()       Start a calibration (Tsys measurement)
-# vlbi_get_calibration()     Collect calibration results
+# -------------------- version 1, obsolete ------------------------------
 
-def vlbi_tpoint():
-    '''Total power pointing for VLBI.'''
-    tp()
-    point(150, time=20)
+def vlbi_tp_onsource_v1(src='M87', t_mins=4):
+    '''Total power monitoring for VLBI, taken while tracking source.
+    
+       t_mins : integration time in MINUTES.
+    '''
+    execfile('pi230_setup.apecs')
+    source(src)
+    #track()   # probably should be changed to "point(<args>)"
+    #on(drift='no', time=30)
+    # Try this...JPE @ 2018-04-19   
+    tp()    
+    repeat(1)
+    use_ref('ON')       
+    reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='EQ', epoch=2000.0)
+    use_ref('OFF')  
+    pi230.configure(doppler='off') 
+    repeat(t_mins)
+    on(drift='no', time=60)
+    repeat(1)
+    track()
+    calibrate(time=5)
+    #use_ref('ON') 
 
-def vlbi_tp_onsource_prepare(src='M87'):
+# -------------------- version 2, obsolete ------------------------------
+
+def vlbi_tp_onsource_prepare_v2(src='M87'):
     execfile('pi230_setup.apecs')
     source(src)
     # Try this...JPE @ 2018-04-19
@@ -31,48 +46,117 @@ def vlbi_tp_onsource_prepare(src='M87'):
     reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='EQ', epoch=2000.0)
     use_ref('OFF')  
 
-def vlbi_tp_onsource(src='M87',t=4):
+def vlbi_tp_onsource_v2(src='M87', t_mins=4):
     '''Total power monitoring for VLBI, taken while tracking source.
     
-       t : integration time in MINUTES.
+       t_mins : integration time in MINUTES.
     '''
+    use_ref('OFF')
     pi230.configure(doppler='off') 
-    repeat(t)
-    on(drift='no',time=60)
+    repeat(t_mins)
+    on(drift='no', time=60)
     repeat(1)
     track()
     # Now get Tsys
     calibrate(time=5)
     #use_ref('ON')
 
-def vlbi_tp_onsource_Old(src='M87',t=4):
+# -------------------- version 2, JPs recommended changes, taken into account in version 3 further below ---------------------------
+
+# Recommendation by JP:
+#  do a 1min calibrate OFF-Source before VLBI scan, if time allows -- use_ref('ON'); reference(x=-100.0, ...); repeat(1); on(drift='no', time=60); 
+#  do the VLBI scan                                                -- use_ref('OFF'); repeat(N_minutes); on(drift='no', time=60):
+#  do a 20s..1min Tsys VLBI scan, if time allows                   -- calibrate(time=5);
+#      move the Tsys to be done after the Off-Source reference cal and _before_ the VLBI scan rather than after
+
+# Example function by JP
+def vlbi_tp_onsource_V2_JP(src='M87', t_mins=4):
     '''Total power monitoring for VLBI, taken while tracking source.
     
-       t : integration time in MINUTES.
+       t_mins : integration time on-source in MINUTES, not including other calibrations (1-2 minutes extra overhead)
     '''
-    execfile('pi230_setup.apecs')
-    source(src)
-    #track()   # probably should be changed to "point(<args>)"
-    #on(drift='no',time=30)
-    # Try this...JPE @ 2018-04-19   
-    tp()    
+    pi230.configure(doppler='off')
+    #
+    # First on() subscan will have a reference
+    #
+    # Set reference in Horizontal mode, so it is at the same elevation as the target source.
+    use_ref('ON')
+    reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='HO', epoch=2000.0)
     repeat(1)
-    use_ref('ON')       
-    reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='EQ', epoch=2000.0)
-    use_ref('OFF')  
-    pi230.configure(doppler='off') 
-    repeat(t)
+    on(drift='no', time=60)
+    
+    #
+    # The next on() subscans will not have a reference to stay on target.
+    # 
+    use_ref('OFF')
+    repeat(t-1)
+    on(drift='no', time=60)
+    
+    #
+    # Just stay on target
+    #
+    repeat(1)
+    track()
+
+    # Now get Tsys
+    calibrate(time=5)
+    #use_ref('ON')
+
+def vlbi_reference_cal():
+    pi230.configure(doppler='off')
+    use_ref('ON')
+    reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='HO', epoch=2000.0)
+    repeat(1)
+    on(drift='no',time=60)
+    use_ref('OFF')
+
+# -------------------- version 3 accounting for JPs suggestions ---------------------------
+
+def vlbi_reference_scan():
+    '''
+    Take an on() scan with duration of ~1 minute with an off-source reference.
+    Prior to calling this function, must already be tracking a source.
+    '''
+    # Do one on() subscan that has a reference
+    # Set reference in Horizontal mode, so it is at the same elevation as the target source.
+    pi230.configure(doppler='off')
+    use_ref('ON')
+    reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='HO', epoch=2000.0)
+    repeat(1)
+    on(drift='no',time=60)
+    use_ref('OFF')
+
+def vlbi_tsys():
+    '''
+    Initiate Tsys measurement. Takes about 4 x 5 seconds to complete.
+    Should ideally be called after vlbi_reference_scan() and
+    before vlbi_scan().
+    '''
+    calibrate(time=5)
+
+def vlbi_scan(t_mins=5):
+    '''
+    The VLBI backends and data recorders will be capturing data during vlbi_scan(),
+    based on their own schedule. This time period is implemented below for APECS
+    as N x 1-minute on() subscans that are on source, without an off-source reference.
+    Once all on() subscans have completed, continue to track() the source.
+
+    Prior to calling this function, must already be tracking a source, and
+    ideally must have called vlbi_reference_scan().
+    '''
+    pi230.configure(doppler='off')
+    use_ref('OFF')
+    repeat(t_mins)
     on(drift='no',time=60)
     repeat(1)
     track()
-    calibrate(time=5)
-    #use_ref('ON')    
-    
+
+# ------------------- probably unused functions:
 
 def vlbi_wpoint(t=20,cal=1):
     '''Wobbler pointing for VLBI.'''
     if (cal):
-       calibrate('cold')
+        calibrate('cold')
 
     wob(75, 1)
     #point(150, time=t)
@@ -84,14 +168,7 @@ def vlbi_focus(axis='Z'):
     vlbi_focus.func_defaults = (axis,)
     wob(75, 1)
     focus(amplitude=1, points=5, axis=axis)
-    tp()
-
-def vlbi_initiate_tsys():
-    '''Start a calibration (Tsys measurement).'''
-    #reference (-1000,0)
-    #calibrate()
-    #reference (0,0)
-    pass
+    tp()    
 
 def vlbi_get_calibration():
     '''Collect calibration results'''
