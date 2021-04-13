f612ad73221cf95c281a983fa6b4ee542d951a0b
t-0107.f-9996a-2021 <t-0107.f-9996a-2021@observer3.apex-telescope.org>
2021-04-13 23:27:32 +0000

    nflash230 vlbi functions with wobbler off, e21a14

--- APECS/2021/vlbi-NFLASH230_commands.py
+++ APECS/2021/vlbi-NFLASH230_commands.py
@@ -9,6 +9,73 @@ import os
 # VLBI Calibration and Scan/VLBI Recording Helpers
 #############################################################################
 
+def vlbi_reference_scan():
+    '''
+    Take an on() scan with duration of 20s (was:~1 minute) with an off-source reference.
+    Prior to calling this function, must already be tracking a source.
+    '''
+
+    nflash230.configure(doppler='off')
+
+    # Set reference in Horizontal mode, so it is at the same elevation as the target source.
+    use_ref('ON')
+    reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='HO', epoch=2000.0)
+
+    # Wobbler off
+    tp()
+
+    # Do one on() subscan that has a reference
+    repeat(1)
+    ## on(drift='no',time=30) # 2 x 30s = 1 minute   ; e21b09 : too long for 2-3min gaps!
+    on(drift='no',time=10) # 2 x 10s = 20 s   # e21b09 - todo ask operators is 10s ok...
+    use_ref('OFF')
+
+    # Stay on target
+    repeat(1)
+    track()
+
+
+def vlbi_tsys():
+    '''
+    Initiate Tsys measurement. Takes about 4 x 5 seconds to complete.
+    Should ideally be called after vlbi_reference_scan() and
+    before vlbi_scan().
+    '''
+
+    reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='HO', epoch=2000.0)
+    calibrate(time=10)
+
+
+def vlbi_scan(t_mins=5):
+    '''
+    Call at the start of a VLBI scan, possibly after vlbi_reference_scan(),
+    while already tracking the VLBI target source.
+
+    Uses a series of on() measurements, because a plain track() does not signal an active
+    observation in APECS and leads to auto-standby of the telescope some minutes later. 
+    The series of on-scans also collect amplitude calibration data for VLBI postprocessing.
+
+    Turns Doppler off, Wobbler off, disables off-source reference position; the telescope
+    must be ON source for the whole  duration of the VLBI scan (~t_mins minutes).
+    '''
+
+    # Tune receiver; no Doppler, no reference position, disable Wobbler (tp() overrides
+    # any wob() that may have persisted from operators line pointing scans)
+    nflash230.configure(doppler='off')
+    use_ref('OFF')
+    tp()
+
+    # Fill most of the VLBI scan time with series of on()
+    repeat(t_mins)
+    # on(drift='no',time=60) # EHT2017, EHT2018
+    on(drift='no',time=30) # EHT2021, changed to 50% of t_mins from middle of e21b09 due to overheads (30%) that are greater than before
+
+    # Continue tracking for remainder of VLBI scan; ought to be less than auto-standby timeout time
+    repeat(1)
+    track()
+
+
+
 # ==EXAMPLE== function by JP below for Tsys measurement
 # JP recommended this process:
 #  do a 1min calibrate OFF-Source before VLBI scan, if time allows -- use_ref('ON'); reference(x=-100.0, ...); repeat(1); on(drift='no', time=60); 
@@ -47,50 +114,6 @@ def vlbi_tp_onsource_JP(src='M87', t_mins=4):
     # Now get Tsys
     calibrate(time=10)
     #use_ref('ON')
-
-
-def vlbi_reference_scan():
-    '''
-    Take an on() scan with duration of 20s (was:~1 minute) with an off-source reference.
-    Prior to calling this function, must already be tracking a source.
-    '''
-    # Do one on() subscan that has a reference
-    # Set reference in Horizontal mode, so it is at the same elevation as the target source.
-    nflash230.configure(doppler='off')
-    use_ref('ON')
-    reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='HO', epoch=2000.0)
-    repeat(1)
-    ## on(drift='no',time=30) # 2 x 30s = 1 minute   ; e21b09 : too long for 2-3min gaps!
-    on(drift='no',time=10) # 2 x 10s = 20 s   # e21b09 - todo ask operators is 10s ok...
-    use_ref('OFF')
-
-
-def vlbi_tsys():
-    '''
-    Initiate Tsys measurement. Takes about 4 x 5 seconds to complete.
-    Should ideally be called after vlbi_reference_scan() and
-    before vlbi_scan().
-    '''
-    calibrate(time=10)
-
-
-def vlbi_scan(t_mins=5):
-    '''
-    The VLBI backends and data recorders will be capturing data during vlbi_scan(),
-    based on their own schedule. This time period is implemented below for APECS
-    as N x 1-minute on() subscans that are on source, without an off-source reference.
-    Once all on() subscans have completed, continue to track() the source.
-
-    Prior to calling this function, must already be tracking a source, and
-    ideally must have called vlbi_reference_scan().
-    '''
-    nflash230.configure(doppler='off')
-    use_ref('OFF')
-    repeat(t_mins)
-    # on(drift='no',time=60)
-    on(drift='no',time=30) ## reduced in middle of e21b09
-    repeat(1)
-    track()
 
 
 # ------------------- probably unused functions:
