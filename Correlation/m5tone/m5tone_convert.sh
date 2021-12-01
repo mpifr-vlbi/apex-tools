21d213f002bfadb6eca298c5a745e651e9155458
oper <oper@mpifr.de>
2021-12-01 15:53:30 +0100

    add mixed to numerical .m5tone text file converter script m5tone_convert.sh

---
+++ Correlation/m5tone/m5tone_convert.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#
+# Convert the mixed text .m5tone files ino numerical-columns-only .tone files
+#
+
+for fn in `ls -1 *.vdif.m5t`; do
+	fout=${fn/.vdif.m5t/.tone}
+	echo $fn $fout
+	cat $fn | cut -c 1-15,24-33,40-51,55-64,70-75 > $fout
+done
