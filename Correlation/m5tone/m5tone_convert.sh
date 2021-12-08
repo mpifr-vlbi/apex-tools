c4367fa8013d9f09e69e650d8e94ebdbba6024ce
oper <oper@mpifr.de>
2021-12-08 14:32:59 +0100

    fix too narrow column in 'cut'

--- Correlation/m5tone/m5tone_convert.sh
+++ Correlation/m5tone/m5tone_convert.sh
@@ -6,5 +6,5 @@
 for fn in `ls -1 *.vdif.m5t`; do
 	fout=${fn/.vdif.m5t/.tone}
 	echo $fn $fout
-	cat $fn | cut -c 1-15,24-33,40-51,55-64,70-75 > $fout
+	cat $fn | cut -c 1-15,22-33,40-51,55-64,70-75 > $fout
 done
