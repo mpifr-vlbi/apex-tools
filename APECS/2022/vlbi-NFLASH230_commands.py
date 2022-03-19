f0ac7d3436329532aca2a3b9a30f633cd27e2a30
t-0109.f-9996a-2022 <t-0109.f-9996a-2022@observer3.apex-telescope.org>
2022-03-19 18:56:07 +0000

    vlbi-NFLASH230_commands.py initial version for e22c20, ensure vlbi_tuning() at every spot, have cats='user' in source() to not block remote mode if same source in multiple catalogs

--- APECS/2022/vlbi-NFLASH230_commands.py
+++ APECS/2022/vlbi-NFLASH230_commands.py
@@ -9,17 +9,39 @@ import os
 # VLBI Calibration and Scan/VLBI Recording Helpers
 #############################################################################
 
+def vlbi_tuning():
+    '''
+    (Re)Configure nflash230.
+
+    This needs to be invoked regulary in case 1) operator interaction left apecs
+    tuned to e.g. CO line instead of vlbi freq, or 2) backend for Tsys was left
+    in continuum rather than in line mode.
+
+    Note: In 2022 we verified CO line pointing works fine while tuned to 'vlbifreq7',
+          nevertheless it remains prudent for the scripted schedule to hammer in the
+          correct VLBI tuning at every opportunity.
+    '''
+
+    setup_nflash( fenames=['nflash230'],
+        linenames=['vlbifreq7'],
+        sidebands=[''], mode='spec', sbwidths=[8], numchan=65536,
+        cats='all',
+        doppler='off' )
+
+    nflash230.configure(doppler='off') # prevent Doppler correction during VLBI scan on()
+    tp()                               # cancel any wob() wobbler config persisting from operator line pointing
+    use_ref('OFF')                     # avoid going off-source during VLBI scan on()
+
+
 def vlbi_tsys(mode_='COLD',time_s=10):
     '''
     Initiate Tsys measurement. Should ideally be called on-target before vlbi_scan().
 
     2022: calibrate(time=6,mode='HOT')    does Sky-Hot without Cold, takes ~40 sec in total
-          calibrate(time=10,mode='COLD')  full Sky-Hot-Cold, takes ~90 sec in total
+          calibrate(time=10,mode='COLD')  full Sky-Hot-Cold, takes ~85 sec in total
     '''
 
-    # EHT2022: added re-tune to 'vlbifreq7' in case operator forgets and apecs still on CO line
-    setup_nflash(fenames=['nflash230'], linenames=['vlbifreq7'], sidebands=[''],mode='spec', sbwidths=[8], numchan=65536, cats='all',doppler='off')
-    
+    vlbi_tuning()
     reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='HO', epoch=2000.0)
     calibrate(mode=mode_,time=time_s)
 
@@ -33,23 +55,19 @@ def vlbi_reference_scan():
           on(drift='no',time=5)  takes 30(?) seconds
     '''
 
-    # Doppler off
-    nflash230.configure(doppler='off')
+    vlbi_tuning()
 
     # Set reference in Horizontal mode, so it is at the same elevation as the target source.
     use_ref('ON')
     reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='HO', epoch=2000.0)
-
-    # Wobbler off
-    tp()
+    tp()  # wobbler off
 
     # Do one on() subscan that has a reference
     repeat(1)
     on(drift='no',time=5) # EHT2022: reduced to 2 x 5sec, from 2 x 10sec/EHT2021, due e22b19 scan gaps being very short
 
+    # Continue tracking VLBI target
     use_ref('OFF')
-
-    # Stay on target
     repeat(1)
     track()
 
@@ -67,33 +85,30 @@ def vlbi_scan(t_mins=5,targetSource=''):
     must be ON source for the whole  duration of the VLBI scan (~t_mins minutes).
     '''
 
+    vlbi_tuning()
+
     if targetSource:
         # If 'targetSource' arg is not empty, make sure we are on that source before starting VLBI scan
         # todo: query current APECS source somehow? to avoid possibly no-op commands?
-        source(targetSource)
+        source(targetSource,cats='user')
         go()
         track()
-
-    # EHT2022: added re-tune to 'vlbifreq7' in case operator forgets and apecs still on CO line
-    setup_nflash(fenames=['nflash230'], linenames=['vlbifreq7'], sidebands=[''],mode='spec', sbwidths=[8], numchan=65536, cats='all',doppler='off')
-
-    # Tune receiver; no Doppler, no reference position, disable Wobbler (tp() overrides
-    # any wob() that may have persisted from operators line pointing scans)
-    nflash230.configure(doppler='off')
-    use_ref('OFF')
-    tp()
 
     # Fill most of the VLBI scan time with series of on()
     repeat(t_mins)
     # on(drift='no',time=60) # EHT2017, EHT2018
     # on(drift='no',time=30) # EHT2021, changed to 50% of t_mins from middle of e21b09 due to overheads (30%) that are greater than before
-    on(drift='no',time=30) # EHT2022 assume same high overhead
+    on(drift='no',time=30)   # EHT2022 assume same high overhead of EHT2021. Worked out okay in e22b19 with 1-5min long scans.
 
     # Continue tracking for remainder of VLBI scan; ought to be less than auto-standby timeout time
     repeat(1)
     track()
 
 
+
+#############################################################################
+# TEST CODE / EXAMPLE CODE
+#############################################################################
 
 # ==EXAMPLE== function by JP below for Tsys measurement
 # JP recommended this process:
