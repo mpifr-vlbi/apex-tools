05a33d164a15de6c8f5453ae0d947602664344ab
oper <712020:769212f2-77b6-4892-bc1e-64bccde54c23@bots.bitbucket.org>
2024-03-25 01:49:55 +0000

    add crotab entry and script for H-maser log polling and regular datetime reconfig

---
+++ Maser/hmaser-cron.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+wget -q ftp://iMaser:ndcu@10.0.2.96/Monitoring/MonitInst.dat -O /home/oper/maser/iMaser56-MonitInst.dat  || true
+wget -q ftp://iMaser:ndcu@10.0.2.96/Monitoring/MonitMin.dat -O /home/oper/maser/iMaser56-MonitMin.dat  || true
+wget -q ftp://iMaser:ndcu@10.0.2.96/Monitoring/MonitMax.dat -O /home/oper/maser/iMaser56-MonitMax.dat  || true
+
+DCMD="SETDATE=`date -u +'%Y/%m/%d'`;"
+TCMD="SETTIME=`date -u +'%H:%M:%S'`;"
+
+echo "Sending date-time commands $DCMD $TCMD"
+echo -e "$DCMD;\r\n" | nc -u -l -w 1 maser.apex-telescope.org 14000 || true
+echo -e "$TCMD;\r\n" | nc -u -l -w 1 maser.apex-telescope.org 14000 || true
+
