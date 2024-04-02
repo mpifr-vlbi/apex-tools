3b1279704ed4853436433044b63e9c9af4d7980a
t-0113.f-9996a-2024 <712020:769212f2-77b6-4892-bc1e-64bccde54c23@bots.bitbucket.org>
2024-04-02 23:47:21 +0000

    tidy up vlbi-NFLASH230_commands.py

--- APECS/apecsVLBI/vlbi-NFLASH230_commands.py
+++ APECS/apecsVLBI/vlbi-NFLASH230_commands.py
@@ -17,13 +17,13 @@ def vlbi_tuning():
     tuned to e.g. CO line instead of vlbi freq, or 2) backend for Tsys was left
     in continuum rather than in line mode.
 
-    Note: In 2022 we verified CO line pointing works fine while tuned to 'vlbifreq7',
+    Note: In 2022 we verified CO line pointing works fine while tuned to 'vlbifreq230',
           nevertheless it remains prudent for the scripted schedule to hammer in the
           correct VLBI tuning at every opportunity.
     '''
 
     setup_nflash( fenames=['nflash230'],
-        linenames=['vlbifreq7'],
+        linenames=['vlbifreq230'],
         sidebands=[''], mode='spec', sbwidths=[8], numchans=[65536],
         cats='all',
         doppler='off' )
@@ -97,64 +97,17 @@ def vlbi_scan(t_mins=5,targetSource=''):
     # Fill most of the VLBI scan time with series of on()
     repeat(t_mins)
     # on(drift='no',time=60) # EHT2017, EHT2018
-    # on(drift='no',time=30) # EHT2021, changed to 50% of t_mins from middle of e21b09 due to overheads (30%) that are greater than before
-    on(drift='no',time=30)   # EHT2022 assume same high overhead of EHT2021. Worked out okay in e22b19 with 1-5min long scans.
+    # on(drift='no',time=30) # EHT2021: changed to 50% of t_mins from middle of e21b09 due to overheads (30%) that are greater than before
+    on(drift='no',time=30)   # EHT2022, EHT2023: assume same high overhead of EHT2021. Worked out okay in e22b19 with 1-5min long scans.
 
     # Continue tracking for remainder of VLBI scan; ought to be less than auto-standby timeout time
     repeat(1)
     track()
 
 
-
 #############################################################################
-# TEST CODE / EXAMPLE CODE
+# probably unused functions
 #############################################################################
-
-# ==EXAMPLE== function by JP below for Tsys measurement
-# JP recommended this process:
-#  do a 1min calibrate OFF-Source before VLBI scan, if time allows -- use_ref('ON'); reference(x=-100.0, ...); repeat(1); on(drift='no', time=60); 
-#  do the VLBI scan                                                -- use_ref('OFF'); repeat(N_minutes); on(drift='no', time=60):
-#  do a 20s..1min Tsys VLBI scan, if time allows                   -- calibrate(time=5);
-#      move the Tsys to be done after the Off-Source reference cal and _before_ the VLBI scan rather than after
-#
-def vlbi_tp_onsource_JP(src='M87', t_mins=4):
-    '''Total power monitoring for VLBI, taken while tracking source.
-    
-       t_mins : integration time on-source in MINUTES, not including other calibrations (1-2 minutes extra overhead)
-    '''
-
-    ## EXAMPLE
-    # UNUSED CODE
-
-    nflash230.configure(doppler='off')
-    #
-    # First on() subscan will have a reference
-    #
-    # Set reference in Horizontal mode, so it is at the same elevation as the target source.
-    use_ref('ON')
-    reference(x=-100.0, y=0.0, time=0.0, on2off=1, unit='arcsec', mode='REL', system='HO', epoch=2000.0)
-    repeat(1)
-    on(drift='no', time=60)
-    
-    #
-    # The next on() subscans will not have a reference to stay on target.
-    # 
-    use_ref('OFF')
-    repeat(t-1)
-    on(drift='no', time=60)
-    
-    #
-    # Just stay on target
-    #
-    repeat(1)
-    track()
-
-    # Now get Tsys
-    calibrate(time=10)
-    #use_ref('ON')
-
-
-# ------------------- probably unused functions:
 
 def vlbi_wpoint(t=20,cal=1):
     '''Wobbler pointing for VLBI.'''
@@ -179,6 +132,7 @@ def vlbi_focus(axis='Z',t=6):
     tp()
     nflash230.configure(doppler='off')  # This brings back the VLBI frequency for the next source (velocity=0)
 
+
 def vlbi_get_calibration():
     '''Collect calibration results'''
     onlineCal = apexObsUtils.getApexCalibrator()
@@ -187,7 +141,8 @@ def vlbi_get_calibration():
     except:
         print 'No calibration result available.'
 
-def vwcpoint(t=24., l=[], cal=1, line='vlbifreq7', dopp='OFF', ptRun=False, dbpcorr=False):
+
+def vwcpoint(t=24., l=[], cal=1, line='vlbifreq230', dopp='OFF', ptRun=False, dbpcorr=False):
     '''
 
     Continuum pointing cross scan in beam switching (wob) mode using pseudocontinumm.
@@ -196,7 +151,7 @@ def vwcpoint(t=24., l=[], cal=1, line='vlbifreq7', dopp='OFF', ptRun=False, dbpc
                   l: Length of the arms of the cross
                      [] = use default value for current FE.
                 cal: 1 = calibrate before the pointing
-               line: 'vlbifreq7' = do pointing at vlbifreq7
+               line: 'vlbifreq230' = do pointing at vlbifreq230
                      '' = do pointing at current frequency
                      '*' = use standard line for current FE.
                dopp: 'ON' apply Doppler correction in the tuning frequency.
