df5534e8db66f912cb070271b316aa6fb1de9350
oper <oper@mpifr.de>
2024-04-30 11:30:14 +0200

    add e22c20 band1 tone extractor config

--- PCal/m5tone/m5tone_eht.c
+++ PCal/m5tone/m5tone_eht.c
@@ -30,6 +30,14 @@
     #define HAVE_LO_OFFSET   1       /* 1: LO offset present and should be removed, 0: disable removal of LO offset */
     #define USE_FAST_LO_OFFSET 1     /* 1: use approximation, de-rotate tone in bin after DFT, faster, 0: no approximation, de-rotate the entire sample stream */
 #endif
+#ifdef STATION_APEX_2022_VDI // BAND 1 tone, 1st LO was a VDI synth that has an LO offset
+    #define VDIF_CHAN_IDX    0       /* VDIF channel that has the tone; 0-based indexing */
+    #define VDIF_CHAN_BW_MHZ 2048    /* Baseband signal bandwidth in MHz */
+    #define TONE_FREQ_MHZ    798.0   /* Baseband tone freq in MHz */
+    #define LO_OFFSET_HZ     +15.022 /* Offset of 1st LO to compensate for */
+    #define HAVE_LO_OFFSET   1       /* 1: LO offset present and should be removed, 0: disable removal of LO offset */
+    #define USE_FAST_LO_OFFSET 1     /* 1: use approximation, de-rotate tone in bin after DFT, faster, 0: no approximation, de-rotate the entire sample stream */
+#endif
 #ifdef STATION_APEX_2018_VDI // BAND 3 tone, 1st LO was a VDI synth that has an LO offset
     #define VDIF_CHAN_IDX    0       /* VDIF channel that has the tone; 0-based indexing */
     #define VDIF_CHAN_BW_MHZ 2048    /* Baseband signal bandwidth in MHz */
