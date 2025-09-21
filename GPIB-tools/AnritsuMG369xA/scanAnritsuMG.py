079284c4f40d3e60a7eeea439686a1485508e285
oper <712020:769212f2-77b6-4892-bc1e-64bccde54c23@bots.bitbucket.org>
2025-09-21 19:34:12 +0000

    add utility that aids determining RX LO offset from PCal tone by stepping through freq offsets in coherence test equipment

---
+++ GPIB-tools/AnritsuMG369xA/scanAnritsuMG.py
@@ -0,0 +1,115 @@
+#!/usr/bin/python
+#
+# Basic control of an Anritsu MG369xA series synthesizer
+# over GPIB - assuming the Anritsu is connected to the LAN
+
+# through a Prologix GPIB-Ethernet converter.
+#
+# The Prologix GPIB-Ethernet converter that needs to be attached to the Anritsu
+# is a commercial product, see https://prologix.biz/, and allows remote
+# access to the GPIB bus. One handy Python package is https://github.com/nelsond/prologix-gpib-ethernet
+#
+# Documentation of the GPIB commands of the Anritsu MG369xA can be found in 
+# "Series MG369XA Synthesized Signal Generator GPIB Programming Manual, P/N: 10370-10354"
+# currently available at
+# https://dl.cdn-anritsu.com/en-us/test-measurement/files/Manuals/Programming-Manual/10370-10354.pdf
+#
+'''
+The remoteAnritsuMG.py <commands> supported commands are
+"freq <xxx.x> mhz", "pow <xxx.x> dbm", "output on|off".
+'''
+
+import argparse
+import time
+
+# $  pip install git+git://github.com/nelsond/prologix-gpib-ethernet.git
+from plx_gpib_ethernet import PrologixGPIBEthernetDevice
+
+class AnritsuMG369xA(PrologixGPIBEthernetDevice):
+
+	def open(self, verbose=True):
+		self.verbose = verbose
+		self.connect()
+		self.reset()
+		self.devname = self.idn()
+		#if self.verbose:
+		print("Connected to %s gpib addr %d device ID %s" % (self.gpib.host, self.address, str(self.devname).strip()))
+
+	def setFrequency(self, f_MHz):
+		cmd = "CF0 %.12f MH" % (f_MHz) # PDF p. 119 - 'Sets CW mode at F0 and opens the F0 parameter.'
+		if self.verbose:
+			print("Sending cmd: %s" % (cmd))
+		self.write(cmd)
+		self.write("CLO")
+
+	def getFrequency(self):
+		qry = "OF0"
+		if self.verbose:
+			print("Sending query: %s" % (qry))
+		fq = self.query(qry)
+		return float(fq) # TODO: any units to parse? scale?
+
+	def getPower(self):
+		self.write("LOG")
+		qry = "OL0"
+		if self.verbose:
+			print("Sending query: %s" % (qry))
+		pwr = self.query(qry)
+		return float(pwr) # TODO: any units to parse? scale?
+
+	def setPower(self, dbm):
+		# PDF p.156
+		if dbm <= 10:
+			self.write("LOG") # 
+			self.write("PU0") # PDF p.208 - 'Selects logarithmic power level operation in dBm.'
+			self.write("L0 %.2f DM" % (dbm))
+			self.write("CLO")
+
+	def rfOff(self):
+		return self.write("RF0") # PDF p.260 - 'Turns off the RF output.'
+
+	def rfOn(self):
+		return self.write("RF1") # PDF p.260 - 'Turns on the RF output.'
+
+	def enableRF(self, ena=False):
+		if not ena:
+			self.rfOff()
+		else:
+			self.rfOn()
+
+	def getActiveSettings(self):
+		f = self.getFrequency()
+		p = self.getPower()
+		rfstate = '??' # todo
+		return "%.12f MHz / %.2f dBm / RF %s" % (f,p,rfstate)
+
+
+parser = argparse.ArgumentParser(description="Anritsu MG369xA basic GPIB remote control via an attached Prologix GPIB-LAN converter", epilog=__doc__)
+parser.add_argument('-H', '--host', dest='host', default='10.0.2.94', help='hostname of the Prologix GPIB-LAN (default %(default)s)')
+parser.add_argument('-a', '--gpib-addr', dest='gpib_addr', default='5', help='address of the MG369xA on the GPIB bus (default %(default)s)')
+parser.add_argument('cmds', nargs='*', help='commands to send')
+args = parser.parse_args()
+
+anritsu = AnritsuMG369xA(host=args.host, address=int(args.gpib_addr))
+anritsu.open(verbose=False)
+print('Current settings: ', anritsu.getActiveSettings())
+
+f0 = 3042.9900
+# f0 = 1042.9900
+# f0 = 4433.9900
+# f0 = 2978.99
+# f0 = 530.99
+
+frange = 0.00004
+fstart = f0 - frange
+fstop =  f0 + frange
+
+f = fstart
+fstep = 1e-6  # 1 Hz as MHz
+while f <= fstop:
+	anritsu.setFrequency(float(f))
+	print('Updated settings: ', anritsu.getActiveSettings(), '- LO offset %.2f ' % ((f-f0)*1e6))
+	f += fstep
+	time.sleep(0.2)
+
+anritsu.close()
