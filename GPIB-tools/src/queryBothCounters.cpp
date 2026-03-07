ed1767797c70d9e60c211fde98316b0d48cd5c0e
oper <712020:769212f2-77b6-4892-bc1e-64bccde54c23@bots.bitbucket.org>
2026-03-07 01:23:27 +0000

    add utilities initBothCounters.cpp queryBothCounters.cpp

---
+++ GPIB-tools/src/queryBothCounters.cpp
@@ -0,0 +1,43 @@
+
+#include "HPCounterGPIB.h"
+#include "PrologixEthGPIB.h"
+
+#include <iostream>
+#include <string>
+#include <vector>
+
+int main(int argc, char** argv)
+{
+   // TODO: command line args?
+
+   const char* prologix_ipv4addr = "10.0.2.93";
+   const int prologix_tcp_port = 1234;
+   int rc;
+
+   PrologixEthGPIB prologix(prologix_ipv4addr, prologix_tcp_port);
+   HPCounterGPIB counter1(prologix, /*gpib addr:*/ 3);
+   HPCounterGPIB counter2(prologix, /*gpib addr:*/ 4);
+
+   rc = prologix.connect();
+   if (rc != 0) {
+      std::cout << "Failed to connect to " << prologix_ipv4addr << " TCP port " << prologix_tcp_port << std::endl;
+      return -1;
+   }
+
+   rc = prologix.configure();
+   if (rc != 0) {
+      std::cout << "Failed to configure Prologix GPIB-LAN converter" << std::endl;
+      return -1;
+   }
+
+   std::cout << "Counter with GPIB address " << counter1.getAddr()
+      << " has ID " << counter1.identify()
+      << " and measurement " << counter1.query("FETCH:TINT?") << std::endl;
+
+   std::cout << "Counter with GPIB address " << counter2.getAddr()
+      << " has ID " << counter2.identify()
+      << " and measurement " << counter1.query("FETCH:TINT?") << std::endl;
+
+   return 0;
+}
+
